d4a8917a518ee56e775a4216cbc87992163c42f0
VOS Maintainer <vos.admin@openlinksw.com>
2006-04-25 21:35:34 +0000

    * Added stored procedure to reset RDF state

--- libsrc/Wi/sparql.sql
+++ libsrc/Wi/sparql.sql
@@ -88,6 +88,25 @@ sequence_set ('RDF_LANGUAGE_TWOBYTE', 258, 1)
 -- create text index on DB.DBA.RDF_OBJ (RO_VAL) with key RO_ID
 -- ;
 
+create procedure DB.DBA.RDF_GLOBAL_RESET ()
+{
+--  checkpoint;
+  __atomic (1);
+  delete from DB.DBA.RDF_QUAD;
+  delete from DB.DBA.RDF_URL;
+  delete from DB.DBA.RDF_OBJ;
+  delete from DB.DBA.RDF_DATATYPE;
+  delete from DB.DBA.RDF_LANGUAGE;  
+  sequence_set ('RDF_URL_IID_NAMED', 1000000, 0);
+  sequence_set ('RDF_URL_IID_BLANK', 1000000000, 0);
+  sequence_set ('RDF_RO_ID', 1, 0);
+  sequence_set ('RDF_DATATYPE_TWOBYTE', 258, 0);
+  sequence_set ('RDF_LANGUAGE_TWOBYTE', 258, 0);
+  __atomic (0);
+--  checkpoint;
+}
+;
+
 -----
 -- Handling of IRI IDs
 
