d12fa39ef7a64d71b089d294c9a1c6d66ddcdefb
VOS Maintainer <vos.admin@openlinksw.com>
2006-09-11 11:10:31 +0000

    * Add vdb exec etc counters in status report

--- libsrc/Wi/srvstat.c
+++ libsrc/Wi/srvstat.c
@@ -122,6 +122,13 @@ unsigned long prof_avg_exec;
 unsigned long  prof_n_compile;
 unsigned long  prof_compile_time;
 
+unsigned long vdb_n_exec;
+unsigned long vdb_n_fetch;
+unsigned long vdb_n_transact;
+unsigned long vdb_n_error;
+
+
+
 long tws_connections;
 long tws_requests;
 long tws_1_1_requests;
@@ -432,7 +439,9 @@ dbms_status_report (void)
   rep_printf ("Log = %s, " OFF_T_PRINTF_FMT " bytes\n",
 	      dbs->dbs_log_name ? dbs->dbs_log_name : "none",
 	      (OFF_T_PRINTF_DTP) dbs->dbs_log_length);
-
+#ifdef UNIVERSE
+  rep_printf ("VDB: %ld exec %ld fetch %ld transact %ld error\n", vdb_n_exec, vdb_n_fetch, vdb_n_transact, vdb_n_error);
+#endif
   rep_printf ("%ld pages have been changed since last backup (in checkpoint state)\n", dbs_count_incbackup_pages (dbs));
 
   bp_curr_ts = bp_curr_timestamp ();
