e4cb725422db9c4edcff4060dd14270777a5af19
VOS Maintainer <vos.admin@openlinksw.com>
2007-10-17 20:49:14 +0000

    * Removed deprecated files

--- appsrc/ODS-Wiki/generate_drop_proc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "select 'WV.WIKI.SILENT_EXEC(''' || 'drop procedure ' || p_name || ''');' from sys_procedures where p_name like 'WV.%.%' and p_name not like 'WV.%.SILENT_EXEC'; " | isql $PORT dba dba | grep drop  > drop_proc.sql
-echo "drop procedure WV.WIKI.SILENT_EXEC;" >>  drop_proc.sql
