69de85acc688395e5d440eabf687a83671f62e56
VOS Maintainer <vos.admin@openlinksw.com>
2007-11-28 12:05:20 +0000

    * Added VD for OWL onology * Fixed URL rewriter rules for rdfbrowser

--- binsrc/samples/demo/load_ontology_dav.sql
+++ binsrc/samples/demo/load_ontology_dav.sql
@@ -1,8 +1,14 @@
 create procedure DB.DBA.LOAD_NW_ONTOLOGY_FROM_DAV()
 {
-  declare content1 varchar;
+  declare content1, urihost varchar;
   select cast (RES_CONTENT as varchar) into content1 from WS.WS.SYS_DAV_RES where RES_FULL_PATH = '/DAV/VAD/demo/sql/nw.owl';
   DB.DBA.RDF_LOAD_RDFXML (content1, 'http://demo.openlinksw.com/schemas/northwind#', 'http://demo.openlinksw.com/schemas/NorthwindOntology/1.0/');
+  urihost := cfg_item_value(virtuoso_ini_path(), 'URIQA','DefaultHost');
+  if (urihost = 'demo.openlinksw.com')
+  {
+    DB.DBA.VHOST_REMOVE (lpath=>'/schemas/northwind');
+    DB.DBA.VHOST_DEFINE (lpath=>'/schemas/northwind', ppath=>'/DAV/VAD/demo/sql/nw.owl', vsp_user=>'dba', is_dav=>1, is_brws=>0);
+  }
 };
 
 DB.DBA.LOAD_NW_ONTOLOGY_FROM_DAV();
