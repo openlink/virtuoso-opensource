243552ccf4aebfbfd5cca9fbcfa7e4c7beea526d
VOS Maintainer <vos.admin@openlinksw.com>
2008-01-22 19:44:37 +0000

    * Merged from upstream project

--- binsrc/oat/toolkit/loader.js
+++ binsrc/oat/toolkit/loader.js
@@ -36,7 +36,7 @@ OAT.Preferences = {
 	xsltPath:"/DAV/JS/xslt/",
 	imagePath:"/DAV/JS/images/",
 	stylePath:"/DAV/JS/styles/",
-	version:"16.01.2008",
+	version:"21.01.2008",
 	httpError:1, /* show http errors */
 	allowDefaultResize:1,
 	allowDefaultDrag:1
