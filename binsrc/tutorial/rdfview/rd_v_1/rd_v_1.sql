512e0218e96ce6df66f35107084557782ec706f9
VOS Maintainer <vos.admin@openlinksw.com>
2008-01-24 13:25:37 +0000

    * Added script loading ontology

--- binsrc/tutorial/rdfview/rd_v_1/rd_v_1.sql
+++ binsrc/tutorial/rdfview/rd_v_1/rd_v_1.sql
@@ -968,3 +968,23 @@ VHOST_REMOVE (lpath=>'/tutorial/Northwind');
 DB.DBA.VHOST_DEFINE (lpath=>'/tutorial/Northwind', ppath=>'/DAV/VAD/tutorial/rdfview/rd_v_1/', vsp_user=>'dba', is_dav=>1, def_page=>'sfront.vspx',
           is_brws=>0, opts=>vector ('url_rewrite', 'tut_nw_rule_list1'));
 
+create procedure DB.DBA.LOAD_TUTNW_ONTOLOGY_FROM_DAV()
+{
+  declare content, urihost varchar;
+  select cast (RES_CONTENT as varchar) into content from WS.WS.SYS_DAV_RES where RES_FULL_PATH = '/DAV/VAD/tutorial/rdfview/rd_v_1/rd_v_1.owl';
+  DB.DBA.RDF_LOAD_RDFXML_MT (content, 'http://demo.openlinksw.com/schemas/tutorial/northwind#', 'http://demo.openlinksw.com/schemas/TutorialNorthwindOntology/1.0/');
+  if (urihost = 'demo.openlinksw.com')
+  {
+    DB.DBA.VHOST_REMOVE (lpath=>'/schemas/tutorial/northwind#');
+    DB.DBA.VHOST_DEFINE (lpath=>'/schemas/tutorial/northwind#', ppath=>'/DAV/VAD/tutorial/rdfview/rd_v_1/rd_v_1.owl', vsp_user=>'dba', is_dav=>1, is_brws=>0);
+    DB.DBA.VHOST_REMOVE (lpath=>'/schemas/tutorial/northwind');
+    DB.DBA.VHOST_DEFINE (lpath=>'/schemas/tutorial/northwind', ppath=>'/DAV/VAD/tutorial/rdfview/rd_v_1/rd_v_1.owl', vsp_user=>'dba', is_dav=>1, is_brws=>0);
+  }
+}
+;
+
+DB.DBA.LOAD_TUTNW_ONTOLOGY_FROM_DAV()
+;
+
+drop procedure DB.DBA.LOAD_TUTNW_ONTOLOGY_FROM_DAV
+;
