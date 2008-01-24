612ba3fc4f49171caafa7b7e0b9529b8817fe793
VOS Maintainer <vos.admin@openlinksw.com>
2008-01-24 13:24:55 +0000

    * Added loading code for ontology

--- binsrc/samples/demo/sql_rdf.sql
+++ binsrc/samples/demo/sql_rdf.sql
@@ -940,3 +940,22 @@ VHOST_REMOVE (lpath=>'/Northwind');
 DB.DBA.VHOST_DEFINE (lpath=>'/Northwind', ppath=>'/DAV/home/demo/', vsp_user=>'dba', is_dav=>1, def_page=>'sfront.vspx',
           is_brws=>0, opts=>vector ('url_rewrite', 'demo_nw_rule_list1'));
 
+create procedure DB.DBA.LOAD_NW_ONTOLOGY_FROM_DAV()
+{
+  declare content1, urihost varchar;
+  select cast (RES_CONTENT as varchar) into content1 from WS.WS.SYS_DAV_RES where RES_FULL_PATH = '/DAV/VAD/demo/sql/nw.owl';
+  DB.DBA.RDF_LOAD_RDFXML (content1, 'http://demo.openlinksw.com/schemas/northwind#', 'http://demo.openlinksw.com/schemas/NorthwindOntology/1.0/');
+  urihost := cfg_item_value(virtuoso_ini_path(), 'URIQA','DefaultHost');
+  if (urihost = 'demo.openlinksw.com')
+  {
+    DB.DBA.VHOST_REMOVE (lpath=>'/schemas/northwind');
+    DB.DBA.VHOST_DEFINE (lpath=>'/schemas/northwind', ppath=>'/DAV/VAD/demo/sql/nw.owl', vsp_user=>'dba', is_dav=>1, is_brws=>0);
+    DB.DBA.VHOST_REMOVE (lpath=>'/schemas/northwind#');
+    DB.DBA.VHOST_DEFINE (lpath=>'/schemas/northwind#', ppath=>'/DAV/VAD/demo/sql/nw.owl', vsp_user=>'dba', is_dav=>1, is_brws=>0);
+  }
+};
+
+DB.DBA.LOAD_NW_ONTOLOGY_FROM_DAV();
+
+drop procedure DB.DBA.LOAD_NW_ONTOLOGY_FROM_DAV;
+
