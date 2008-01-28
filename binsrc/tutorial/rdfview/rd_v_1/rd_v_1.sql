899680136cb7150f79924b79d958ce6314b347a5
VOS Maintainer <vos.admin@openlinksw.com>
2008-01-28 09:43:16 +0000

    * Fixed check for missing ontology file

--- binsrc/tutorial/rdfview/rd_v_1/rd_v_1.sql
+++ binsrc/tutorial/rdfview/rd_v_1/rd_v_1.sql
@@ -971,6 +971,7 @@ DB.DBA.VHOST_DEFINE (lpath=>'/tutorial/Northwind', ppath=>'/DAV/VAD/tutorial/rdf
 create procedure DB.DBA.LOAD_TUTNW_ONTOLOGY_FROM_DAV()
 {
   declare content, urihost varchar;
+  whenever not found goto endpoint;
   select cast (RES_CONTENT as varchar) into content from WS.WS.SYS_DAV_RES where RES_FULL_PATH = '/DAV/VAD/tutorial/rdfview/rd_v_1/rd_v_1.owl';
   DB.DBA.RDF_LOAD_RDFXML_MT (content, 'http://demo.openlinksw.com/schemas/tutorial/northwind#', 'http://demo.openlinksw.com/schemas/TutorialNorthwindOntology/1.0/');
   if (urihost = 'demo.openlinksw.com')
@@ -980,6 +981,8 @@ create procedure DB.DBA.LOAD_TUTNW_ONTOLOGY_FROM_DAV()
     DB.DBA.VHOST_REMOVE (lpath=>'/schemas/tutorial/northwind');
     DB.DBA.VHOST_DEFINE (lpath=>'/schemas/tutorial/northwind', ppath=>'/DAV/VAD/tutorial/rdfview/rd_v_1/rd_v_1.owl', vsp_user=>'dba', is_dav=>1, is_brws=>0);
   }
+  endpoint:
+  ;
 }
 ;
 
