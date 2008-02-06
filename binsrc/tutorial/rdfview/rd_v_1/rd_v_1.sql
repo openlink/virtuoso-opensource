9f4754638beeca8048a15450b2ec568a297f3126
VOS Maintainer <vos.admin@openlinksw.com>
2008-02-06 23:24:24 +0000

    * Fixed problems with vad install

--- binsrc/tutorial/rdfview/rd_v_1/rd_v_1.sql
+++ binsrc/tutorial/rdfview/rd_v_1/rd_v_1.sql
@@ -973,7 +973,9 @@ create procedure DB.DBA.LOAD_TUTNW_ONTOLOGY_FROM_DAV()
   declare content, urihost varchar;
   whenever not found goto endpoint;
   select cast (RES_CONTENT as varchar) into content from WS.WS.SYS_DAV_RES where RES_FULL_PATH = '/DAV/VAD/tutorial/rdfview/rd_v_1/rd_v_1.owl';
-  DB.DBA.RDF_LOAD_RDFXML_MT (content, 'http://demo.openlinksw.com/schemas/tutorial/northwind#', 'http://demo.openlinksw.com/schemas/TutorialNorthwindOntology/1.0/');
+  if (content is null or content = '')
+    goto endpoint;
+  DB.DBA.RDF_LOAD_RDFXML (content, 'http://demo.openlinksw.com/schemas/tutorial/northwind#', 'http://demo.openlinksw.com/schemas/TutorialNorthwindOntology/1.0/');
   if (urihost = 'demo.openlinksw.com')
   {
     DB.DBA.VHOST_REMOVE (lpath=>'/schemas/tutorial/northwind#');
