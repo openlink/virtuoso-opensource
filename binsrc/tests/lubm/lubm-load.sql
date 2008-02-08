96f1111b3a6188eb251ed5992bf20e52c0c897ca
VOS Maintainer <vos.admin@openlinksw.com>
2008-02-08 16:45:01 +0000

    * Added code sample for faster suborg materialization

--- binsrc/tests/lubm/lubm-load.sql
+++ binsrc/tests/lubm/lubm-load.sql
@@ -25,8 +25,16 @@ ECHO BOTH $IF $EQU $LAST[1] 100545 "PASSED" "***FAILED";
 SET ARGV[$LIF] $+ $ARGV[$LIF] 1;
 ECHO BOTH ": Data loaded: " $LAST[1] " rows\n";
 
+/* To materialize suborg in a large database:
+log_enable (2);
+insert soft rdf_quad (g,s,p,o)
+select iri_to_id ('lubm', 0), "x", iri_to_id ('http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#subOrganizationOf', 0), "z" from (
+sparql define output:valmode "LONG" prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+select   ?x   ?z   from <lubm> where { ?x ub:subOrganizationOf ?y . ?y ub:subOrganizationOf ?z . }) c;
+
+*/
 sparql prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
-insert into graph <lubm> { ?x ub:subOrganizationOf ?z  } where { ?x ub:subOrganizationOf ?y . ?y ub:subOrganizationOf ?z . };
+insert into graph <lubm> { ?x ub:subOrganizationOf ?z  } from <lubm> where { ?x ub:subOrganizationOf ?y . ?y ub:subOrganizationOf ?z . };
 
 DB.DBA.TTLP (file_to_string ('inf.nt'), 'http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl', 'inf');
 sparql select count(*) from <inf> where { ?x ?y ?z } ;
@@ -36,3 +44,4 @@ ECHO BOTH ": Inference loaded: " $LAST[1] " rows\n";
 
 rdfs_rule_set ('inft', 'inf');
 
+checkpoint;
