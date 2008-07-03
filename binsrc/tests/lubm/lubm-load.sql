d87b49600efe9aaebdae14641f1343b9a43f43d5
VOS Maintainer <vos.admin@openlinksw.com>
2008-07-03 14:10:47 +0000

    * Fixed comment

--- binsrc/tests/lubm/lubm-load.sql
+++ binsrc/tests/lubm/lubm-load.sql
@@ -27,14 +27,14 @@ ECHO BOTH $IF $EQU $LAST[1] 100545 "PASSED" "***FAILED";
 SET ARGV[$LIF] $+ $ARGV[$LIF] 1;
 ECHO BOTH ": Data loaded: " $LAST[1] " rows\n";
 
-/* To materialize suborg in a large database:
-log_enable (2);
-insert soft rdf_quad (g,s,p,o)
-select iri_to_id ('lubm', 0), "x", iri_to_id ('http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#subOrganizationOf', 0), "z" from (
-sparql define output:valmode "LONG" prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
-select   ?x   ?z   from <lubm> where { ?x ub:subOrganizationOf ?y . ?y ub:subOrganizationOf ?z . }) c;
+-- To materialize suborg in a large database:
+--  log_enable (2);
+--  insert soft rdf_quad (g,s,p,o)
+--  select iri_to_id ('lubm', 0), "x", iri_to_id ('http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#subOrganizationOf', 0), "z" from (
+--  sparql define output:valmode "LONG" prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
+--  select   ?x   ?z   from <lubm> where { ?x ub:subOrganizationOf ?y . ?y ub:subOrganizationOf ?z . }) c;
+-- 
 
-*/
 sparql prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
 insert into graph <lubm> { ?x ub:subOrganizationOf ?z  } from <lubm> where { ?x ub:subOrganizationOf ?y . ?y ub:subOrganizationOf ?z . };
 
