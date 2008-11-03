79d6ace2bcef8b53cbb512d7c7627bdabf2af2b2
VOS Maintainer <vos.admin@openlinksw.com>
2008-11-03 12:58:16 +0000

    * Updated version to 05.09

--- libsrc/Wi/sqlver.h
+++ libsrc/Wi/sqlver.h
@@ -32,7 +32,7 @@
 /* DBMS Server */
 #define PRODUCT_DBMS		PRODUCT_NAME
 #define DBMS_SRV_NAME		PRODUCT_DBMS " Universal Server"
-#define DBMS_SRV_VER_ONLY	"05.08"
+#define DBMS_SRV_VER_ONLY	"05.09"
 #define DBMS_SRV_GEN_MAJOR	"30"
 #define DBMS_SRV_GEN_MINOR	"35"
 #define DBMS_SRV_VER		DBMS_SRV_VER_ONLY "." \
