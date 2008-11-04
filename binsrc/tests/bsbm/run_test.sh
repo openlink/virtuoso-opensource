e5fcb5f2463d9615d1cdf9ae8ec225da6c62b79f
VOS Maintainer <vos.admin@openlinksw.com>
2008-11-04 16:03:04 +0000

    * Reverted last patch

--- binsrc/tests/bsbm/run_test.sh
+++ binsrc/tests/bsbm/run_test.sh
@@ -1,11 +1,8 @@
-export PATH=/usr/local/jdk1.6.0_07-64/bin:$PATH
-
-HTTPPORT=8890
-TEST_DRIVER='java -Xmx2048m -cp bin:lib/ssj.jar:lib/log4j-1.2.15.jar:lib/virtjdbc3.jar benchmark.testdriver.TestDriver'
+HTTPPORT=`expr $PORT + 7000`
+TEST_DRIVER='/usr/java/latest/bin/java -Xmx2048m -cp bin:lib/ssj.jar:lib/log4j-1.2.15.jar:lib/virtjdbc3.jar benchmark.testdriver.TestDriver'
 
 #Plain WS run
 #$TEST_DRIVER -runs 128 -w 32 -virtuoso -dg BSBM http://localhost:8310/sparql
 
 #Plain Virtuoso SQL run
-#strace -o xx -s 128 -ff $TEST_DRIVER -runs 10 -w 10 -dg BSBM -dbconnect -sql -virtuoso -qdir SQLqueries -idir datadir-70812 -dbdriver virtuoso.jdbc3.Driver -connuser dba -connpwd dba jdbc:virtuoso://localhost/UID=dba/PWD=dba $*
-$TEST_DRIVER -runs 128 -w 32 -dg BSBM -dbconnect -sql -virtuoso -qdir SQLqueries -idir datadir-2785 -dbdriver virtuoso.jdbc3.Driver -connuser dba -connpwd dba jdbc:virtuoso://localhost/UID=dba/PWD=dba $*
+$TEST_DRIVER -runs 128 -w 32 -dg BSBM -dbconnect -sql -virtuoso -qdir SQLqueries -dbdriver virtuoso.jdbc3.Driver -connuser dba -connpwd dba jdbc:virtuoso://localhost:$PORT/UID=dba/PWD=dba
