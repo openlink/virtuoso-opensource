c26bf4cf074fbbeab0ff562e6babd4b2dc420ad5
VOS Maintainer <vos.admin@openlinksw.com>
2008-11-13 10:02:45 +0000

    * Fixed codegen for cast of SSG_VALMODE_LONG to short of multipart   sprintf IRI class

--- libsrc/Wi/sparql.sql
+++ libsrc/Wi/sparql.sql
@@ -11248,7 +11248,7 @@ create procedure DB.DBA.SPARQL_RELOAD_QM_GRAPH ()
   if (not exists (sparql define input:storage "" ask where {
           graph <http://www.openlinksw.com/schemas/virtrdf#> {
               <http://www.openlinksw.com/sparql/virtrdf-data-formats.ttl>
-                virtrdf:version '2008-10-16 0001'
+                virtrdf:version '2008-11-12 0001'
             } } ) )
     {
       declare txt1, txt2 varchar;
