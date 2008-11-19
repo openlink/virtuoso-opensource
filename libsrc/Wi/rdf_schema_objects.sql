b5322c964795bb32418bc2bf17ed731606f45ec8
VOS Maintainer <vos.admin@openlinksw.com>
2008-11-19 11:23:18 +0000

    * Added generation of OWL and VDs from DB schema

--- libsrc/Wi/rdf_schema_objects.sql
+++ libsrc/Wi/rdf_schema_objects.sql
@@ -80,7 +80,7 @@ rdf_view_create_view (in qualifier varchar, in _tbls any)
    for (declare xx any, xx := 0; xx < length (_tbls) ; xx := xx + 1)
       {
 	   tbl := _tbls[xx];
-	   tbl_name := name_part (tbl, 3);
+	   tbl_name := name_part (tbl, 2);
 	   tbl_name_l := lcase (tbl_name);
 	   tname := tbl_name_l || suffix;
 
@@ -93,11 +93,9 @@ rdf_view_create_view (in qualifier varchar, in _tbls any)
 	       ret := ret || rdf_view_sp (6) || sprintf ('%s:%s %s.%s as virtrdf:%s-%s ;\n',
 				qualifier, lcase("COLUMN"), tname, "COLUMN", tbl_name_l, lcase("COLUMN") );
 	     }
-	   if (
-	       exists (select top 1 1 from SYS_FOREIGN_KEYS where PK_TABLE = tbl and 0 < position (FK_TABLE, _tbls))
+	   if (exists (select top 1 1 from SYS_FOREIGN_KEYS where PK_TABLE = tbl and 0 < position (FK_TABLE, _tbls))
 	       or
-	       exists (select top 1 1 from SYS_FOREIGN_KEYS where FK_TABLE = tbl and 0 < position (FK_TABLE, _tbls))
-	       )
+	       exists (select top 1 1 from SYS_FOREIGN_KEYS where FK_TABLE = tbl and 0 < position (PK_TABLE, _tbls)))
 	   ret := ret || rdf_view_sp (6) || '# Maps from foreign-key relations of ' || tbl || '\n';
 	   ret := ret || rdf_view_get_fk_pk_rel (qualifier, suffix, tbl, _tbls);
 	   ret := ret || rdf_view_get_pk_fk_rel (qualifier, suffix, tbl, _tbls);
@@ -185,8 +183,12 @@ rdf_view_dv_to_printf_str_type (in _dv varchar)
    if (_dv = 189 or _dv = 188) return '%d';
    else if (_dv = 182) return '%U';
    else if (__tag of float = _dv) return '%f';
+   else if (__tag of real = _dv) return '%f';
    else if (__tag of numeric = _dv) return '%f';
-   signal ('42000', sprintf ('Unsupported data type %i in rdf_view_dv_to_printf_str_type', _dv));
+   else if (__tag of date = _dv) return '%1D';
+   else if (__tag of time = _dv) return '%1D';
+   else if (__tag of datetime = _dv) return '%1D';
+   signal ('42000', sprintf ('The current implementation do no supports data type %s (%i) for IRI classes', dv_type_title (_dv), _dv));
 }
 ;
 
@@ -194,15 +196,19 @@ create procedure
 rdf_view_dv_to_sql_str_type (in _dv varchar)
 {
    if (_dv = 189 or _dv = 188) return 'integer';
-   else if (_dv = 182) return 'varchar';
+   else if (_dv = 182 or _dv = 125 or _dv = 131) return 'varchar';
    else if (__tag of float = _dv) return 'float';
+   else if (__tag of real = _dv) return 'float';
    else if (__tag of numeric = _dv) return 'numeric';
-   signal ('42000', sprintf ('Unsupported data type %i', _dv));
+   else if (__tag of date = _dv) return 'date';
+   else if (__tag of time = _dv) return 'time';
+   else if (__tag of datetime = _dv) return 'datetime';
+   signal ('42000', sprintf ('The current implementation do no supports data type %s (%i) for IRI classes', dv_type_title (_dv), _dv));
 }
 ;
 
 create procedure
-rdf_view_create_class (in _tbl varchar, in _host varchar, in _f varchar)
+rdf_view_create_class (in _tbl varchar, in _host varchar, in qualifier varchar)
 {
    declare ret, qual, tbl_name, tbl_name_l, pks, pk_text, sk_str any;
 
@@ -224,7 +230,7 @@ rdf_view_create_class (in _tbl varchar, in _host varchar, in _f varchar)
    pk_text := trim (pk_text, ',');
    sk_str  := trim (sk_str , '/');
    ret := sprintf ('create iri class %s:%s "http://%s/%s/%s/%s#this" (%s) . \n',
-		_f, tbl_name_l, _host, _f, tbl_name_l, sk_str, pk_text);
+		qualifier, tbl_name_l, _host, qualifier, tbl_name_l, sk_str, pk_text);
    return ret;
 }
 ;
@@ -239,7 +245,7 @@ rdf_view_get_primary_key (in _tbl varchar)
 create procedure
 rdf_view_get_relations (in _tbls varchar, in _suff varchar)
 {
-   declare ret, tbl any;
+   declare ret any;
 
    ret := '';
    foreach (any tbl in _tbls) do
@@ -262,3 +268,126 @@ rdf_view_get_relations (in _tbls varchar, in _suff varchar)
 }
 ;
 
+create procedure
+RDF_OWL_FROM_TBL (in qual varchar, in _tbls any)
+{
+  declare ses any;
+  declare ns varchar;
+  ns := sprintf ('@prefix %s: <http://%s/%s#> .\n', qual, cfg_item_value(virtuoso_ini_path(), 'URIQA','DefaultHost'), qual);
+  ses := string_output ();
+  http ('@prefix owl: <http://www.w3.org/2002/07/owl#> .\n', ses);
+  http ('@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n', ses);
+  http ('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n', ses);
+  http ('@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n', ses);
+  http (ns, ses);
+  http (sprintf ('\n%s: a owl:Ontology .\n', qual), ses);
+  foreach (varchar tbl in _tbls) do
+    {
+      http (sprintf ('\n# %s\n', tbl), ses);
+      http (sprintf ('%s:%s a rdfs:Class .\n', qual, name_part (tbl, 2)), ses);
+      http (sprintf ('%s:%s rdfs:label "%s" .\n', qual, name_part (tbl, 2), tbl), ses);
+      for select "COLUMN" as col, COL_DTP as dtp from SYS_COLS where "TABLE" = tbl order by COL_ID do
+	{
+	  declare xsd varchar;
+	  col := lower (col);
+	  http (sprintf ('%s:%s a rdf:Property .\n', qual, col), ses);
+	  http (sprintf ('%s:%s rdfs:domain %s:%s .\n', qual, col, qual, name_part (tbl, 2)), ses);
+	  xsd := rdf_view_dv_to_sql_str_type (dtp);
+	  http (sprintf ('%s:%s rdfs:range xsd:%s .\n', qual, col, xsd), ses);
+	  http (sprintf ('%s:%s rdfs:label "%s" .\n', qual, col, col), ses);
+	}
+      for select distinct PK_TABLE as pkt from SYS_FOREIGN_KEYS where FK_TABLE = tbl and 0 < position (PK_TABLE, _tbls) do
+	{
+	  http (sprintf ('%s:has_%s a rdf:Property .\n', qual, lcase (name_part (pkt, 2))), ses);
+	  http (sprintf ('%s:has_%s rdfs:domain %s:%s .\n', qual, lcase (name_part (pkt, 2)), qual, name_part (tbl, 2)), ses);
+	  http (sprintf ('%s:has_%s rdfs:range %s:%s .\n', qual, lcase (name_part (pkt, 2)), qual, name_part (pkt, 2)), ses);
+	  http (sprintf ('%s:has_%s rdfs:label "Relation to %s" .\n', qual, lcase (name_part (pkt, 2)), pkt), ses);
+	}
+      for select distinct FK_TABLE as pkt from SYS_FOREIGN_KEYS where PK_TABLE = tbl and 0 < position (FK_TABLE, _tbls) do
+	{
+	  http (sprintf ('%s:%s_of a rdf:Property .\n', qual, lcase (name_part (tbl, 2))), ses);
+	  http (sprintf ('%s:%s_of rdfs:domain %s:%s .\n', qual, lcase (name_part (tbl, 2)), qual, name_part (tbl, 2)), ses);
+	  http (sprintf ('%s:%s_of rdfs:range %s:%s .\n', qual, lcase (name_part (tbl, 2)), qual, name_part (pkt, 2)), ses);
+	  http (sprintf ('%s:has_%s rdfs:label "Relation to %s" .\n', qual, lcase (name_part (tbl, 2)), pkt), ses);
+	}
+    }
+  return string_output_string (ses);
+}
+;
+
+create procedure RDF_VIEW_GEN_VD (in qual varchar)
+{
+  declare ses, pref any;
+  ses := string_output ();
+  pref := lower (qual);
+
+  if (
+      exists (select 1 from URL_REWRITE_RULE where URR_RULE = pref || '_rule1') or
+      exists (select 1 from URL_REWRITE_RULE where URR_RULE = pref || '_rule2') or
+      exists (select 1 from URL_REWRITE_RULE where URR_RULE = pref || '_rule3') or
+      exists (select 1 from URL_REWRITE_RULE_LIST where URRL_LIST = pref || '_rule_list1') or
+      exists (select 1 from HTTP_PATH where HP_HOST = '*ini*' and HP_LISTEN_HOST = '*ini*' and HP_LPATH = '/'||qual)
+      )
+    return '\n-- WARNING: there are already created virtual directory "/'||qual||'", skipping virtual directory generation\n'||
+    '-- WARNING: To avoid this message chose different base URL or drop existing virtual directory and its rewrite rules.\n';
+
+  http (
+  'DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+    ''<pref>_rule2'',
+    1,
+    ''(/[^#]*)'',
+    vector(''path''),
+    1,
+    ''/sparql?query=DESCRIBE+%%3Chttp%%3A//^{URIQADefaultHost}^%U%%23this%%3E+%%3Chttp%%3A//^{URIQADefaultHost}^%U%%3E+FROM+%%3Chttp%%3A//^{URIQADefaultHost}^/<pref>%%3E&format=%U'',
+    vector(''path'', ''path'', ''*accept*''),
+    null,
+    ''(text/rdf.n3)|(application/rdf.xml)'',
+    2,
+    null
+    );', ses);
+
+  http ('\n', ses);
+
+  http (
+  'DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+    ''<pref>_rule1'',
+    1,
+    ''([^#]*)'',
+    vector(''path''),
+    1,
+    ''/about/html/http://^{URIQADefaultHost}^%s'',
+    vector(''path''),
+    null,
+    null,
+    2,
+    303
+    );', ses);
+  http ('\n', ses);
+  http('DB.DBA.URLREWRITE_CREATE_REGEX_RULE (
+      ''<pref>_rule3'',
+      1,
+      ''/<qual>\x24'',
+      vector(),
+      1,
+      ''/sparql?query=CONSTRUCT+{+%%3fs+%%3fp+%%3fo+}+FROM+%%3Chttp%%3A//^{URIQADefaultHost}^/schemas/<qual>%%23%%3E+WHERE{%%3fs+%%3fp+%%3fo}&format=%U'',
+      vector(''*accept*''),
+      null,
+      ''(text/rdf.n3)|(application/rdf.xml)'',
+      2,
+      null
+      );', ses);
+  http ('\n', ses);
+  http ('DB.DBA.URLREWRITE_CREATE_RULELIST ( ''<pref>_rule_list1'', 1, vector ( ''<pref>_rule1'', ''<pref>_rule2'', ''<pref>_rule3''));', ses);
+
+  http ('\n', ses);
+  http ('DB.DBA.VHOST_REMOVE (lpath=>''/<qual>'');', ses);
+  http ('\n', ses);
+  http('DB.DBA.VHOST_DEFINE (lpath=>''/<qual>'', ppath=>''/'', vsp_user=>''dba'', is_dav=>0,
+          is_brws=>0, opts=>vector (''url_rewrite'', ''<pref>_rule_list1'')
+	  );',ses);
+   ses := string_output_string (ses);
+   ses := replace (ses, '<pref>', pref);
+   ses := replace (ses, '<qual>', qual);
+   return ses;
+}
+;
