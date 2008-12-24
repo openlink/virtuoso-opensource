ace538e793ef0b85554477818e4fdfcfdf3c5038
VOS Maintainer <vos.admin@openlinksw.com>
2008-12-24 00:12:50 +0000

    * Added oat tree into vad_version calculation

--- binsrc/isparql/make_vad.sh
+++ binsrc/isparql/make_vad.sh
@@ -92,7 +92,11 @@ VERSION_INIT()
       for i in `find . -name 'Entries' | grep -v "vad/" | grep -v "toolkit[a-z\-_]*/"`; do
 	  cat $i | grep "^[^D].*" | cut -f 3 -d "/" | sed -e "s/1\.//g" >> version.tmp
       done
-      VERSION=`cat version.tmp | awk ' BEGIN { cnt=637 } { cnt = cnt + $1 } END { printf "1.%02.02f", cnt/100 }'`
+      # Also reflect changes in oat
+      for i in `find ../oat -name 'Entries' `; do
+	  cat "$i" | grep -v "version\."| grep "^[^D].*" | cut -f 3 -d "/" | sed -e "s/1\.//g" >> version.tmp
+      done
+      VERSION=`cat version.tmp | awk ' BEGIN { cnt=0 } { cnt = cnt + $1 } END { printf "1.%02.02f", cnt/100 }'`
       rm -f version.tmp
       echo "$VERSION" > vad_version
   fi
