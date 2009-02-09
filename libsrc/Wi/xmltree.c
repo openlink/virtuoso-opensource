6bdd1b510e2cc59fbe477a92cc2d4090c5f162f4
VOS Maintainer <vos.admin@openlinksw.com>
2009-02-09 11:55:49 +0000

    * Fixed handling of WIDE data

--- libsrc/Wi/xmltree.c
+++ libsrc/Wi/xmltree.c
@@ -9565,7 +9565,7 @@ caddr_t bif_xsd_type (caddr_t * qst, caddr_t * err_ret, state_slot_t ** args)
         case DT_TYPE_TIME: return uname_xmlschema_ns_uri_hash_time;
         default : return uname_xmlschema_ns_uri_hash_dateTime;
         }
-    case DV_STRING: case DV_BLOB_HANDLE:
+    case DV_STRING: case DV_BLOB_HANDLE: case DV_WIDE: case DV_LONG_WIDE:
       if (1 < BOX_ELEMENTS (args))
         {
           caddr_t dflt = bif_arg (qst, args, 1, "__xsd_type");
