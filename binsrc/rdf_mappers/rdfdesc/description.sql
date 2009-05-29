79d10b7d8db54376ac4b20ab5bc2b04011b44c44
VOS Maintainer <vos.admin@openlinksw.com>
2009-05-29 12:59:04 +0000

    * Fixed scale images to 160px height * Fixed minor issues

--- binsrc/rdf_mappers/rdfdesc/description.sql
+++ binsrc/rdf_mappers/rdfdesc/description.sql
@@ -102,7 +102,7 @@ create procedure rdfdesc_rel_print (in val any, in rel any, in flag int := 0)
   delim := __max (delim1, delim2, delim3);
   nss := null;
   loc := val;
-  if (delim < 0) return;
+  if (delim < 0) return loc;
   nss := subseq (val, 0, delim + 1);
   loc := subseq (val, delim + 1);
 
@@ -254,7 +254,7 @@ again:
 
        rdfa := rdfdesc_rel_print (prop, rel, 0);
        if (http_mime_type (_url) like 'image/%')
-	 http (sprintf ('<a class="uri" %s href="%s"><img src="%s" border="0"/></a>', rdfa, rdfdesc_http_url (_url), _url));
+	 http (sprintf ('<a class="uri" %s href="%s"><img src="%s" height="160" border="0"/></a>', rdfa, rdfdesc_http_url (_url), _url));
        else
        http (sprintf ('<a class="uri" %s href="%s">%s</a>', rdfa, rdfdesc_http_url (_url), rdfdesc_uri_curie(_url, _label)));
 
