13627c822dd254325b398cca9ad50d6f477a5258
VOS Maintainer <vos.admin@openlinksw.com>
2009-06-08 13:16:04 +0000

    * Fixed ttl serialization

--- libsrc/Wi/rdfbox.c
+++ libsrc/Wi/rdfbox.c
@@ -1403,7 +1403,7 @@ ttl_http_write_prefix_if_needed (caddr_t *qst, dk_session_t *ses, ttl_env_t *env
       if (NULL == ti->uri)
         {
           ti->uri = ti->ns;
-          ti->ns = NULL;
+          ti->ns = uname___empty;
         }
       return 0;
     }
