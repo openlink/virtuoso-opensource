088b2db68a19344c60fe4bc6faff63ca50b3d6cd
VOS Maintainer <vos.admin@openlinksw.com>
2009-08-05 09:14:58 +0000

    * Fixed refuse to run without proper cli_client

--- libsrc/Wi/aqueue.c
+++ libsrc/Wi/aqueue.c
@@ -420,7 +420,7 @@ aq_sql_func (caddr_t * av, caddr_t * err_ret)
 	  return NULL;
 	}
     }
-  if (!sec_proc_check (proc, cli->cli_user->usr_id, cli->cli_user->usr_g_id))
+  if (!cli->cli_user || !sec_proc_check (proc, cli->cli_user->usr_id, cli->cli_user->usr_g_id))
     {
       *err_ret = srv_make_new_error ("42000", "SR186", "No permission to execute %s in aq_request", full_name);
       dk_free_tree ((caddr_t) params);
