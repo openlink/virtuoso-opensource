3c5d89f422d845cadc29631b0d9e35f0e4481e8d
VOS Maintainer <vos.admin@openlinksw.com>
2009-08-17 22:36:18 +0000

    * Fixed compiler warnings

--- libsrc/Wi/rdfbox.c
+++ libsrc/Wi/rdfbox.c
@@ -2019,7 +2019,7 @@ bif_sparql_rset_xml_write_row (caddr_t * qst, caddr_t * err_ret, state_slot_t **
                     if (NULL == iri)
                       {
                         char buf[50];
-                        sprintf (buf, "bad://" BOXINT_FMT, (boxint)iri);
+                        snprintf (buf, sizeof (buf), "bad://" BOXINT_FMT, (boxint)(ptrlong)iri);
                         SES_PRINT (ses, buf);
                       }
                     else
@@ -2040,7 +2040,7 @@ bif_sparql_rset_xml_write_row (caddr_t * qst, caddr_t * err_ret, state_slot_t **
                 if (NULL == iri)
                   {
                     char buf[50];
-                    sprintf (buf, "bad://" BOXINT_FMT, (boxint)iri);
+                    snprintf (buf, sizeof (buf), "bad://" BOXINT_FMT, (boxint)(ptrlong)iri);
                     SES_PRINT (ses, buf);
                   }
                 else
