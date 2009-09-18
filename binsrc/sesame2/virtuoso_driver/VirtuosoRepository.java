7c0d4a76a1ee1b9b501b3611a7ac03a306c43d1e
VOS Maintainer <vos.admin@openlinksw.com>
2009-09-18 09:07:08 +0000

    * Fixed Sesame 2 and 3 providers to require JDK 1.6 and virtjdbc4.jar

--- binsrc/sesame2/virtuoso_driver/VirtuosoRepository.java
+++ binsrc/sesame2/virtuoso_driver/VirtuosoRepository.java
@@ -32,7 +32,7 @@ import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 
-import virtuoso.jdbc3.VirtuosoConnectionPoolDataSource;
+import virtuoso.jdbc4.VirtuosoConnectionPoolDataSource;
 
 /**
  * A Sesame repository that contains RDF data that can be queried and updated.
@@ -203,7 +203,7 @@ public class VirtuosoRepository implements Repository {
 	public RepositoryConnection getConnection() throws RepositoryException {
 		if (url_hostlist.startsWith("jdbc:virtuoso://")) {
 			try {
-				Class.forName("virtuoso.jdbc3.Driver");
+				Class.forName("virtuoso.jdbc4.Driver");
 				String url = url_hostlist;
 				if (url.indexOf(utf8) == -1) {
 	   				if (url.charAt(url.length()-1) != '/')
