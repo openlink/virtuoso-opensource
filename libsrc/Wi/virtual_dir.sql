1a550e15503999b511a6ac30f5f682b9de021b28
VOS Maintainer <vos.admin@openlinksw.com>
2009-10-05 19:11:30 +0000

    * Fixed check for longer idn first

--- libsrc/Wi/virtual_dir.sql
+++ libsrc/Wi/virtual_dir.sql
@@ -1072,7 +1072,7 @@ end_loop:;
 	  else
 	    login := '';
 	  host := http_request_header(http_request_header(), 'Host', null, null);
-	  ids := vector ('rdf', 'id', 'id/entity');
+	  ids := vector ('rdf', 'id/entity', 'id');
 	  foreach (varchar idn in ids) do
 	    {
 	      pref := 'http://' || host || http_map_get ('domain') || '/' || idn || '/';
