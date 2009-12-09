5039935832546e3b98204cc7e25f066462a73dd6
VOS Maintainer <vos.admin@openlinksw.com>
2009-12-09 13:35:02 +0000

    * Fixed headers and copyright

--- libsrc/JDBCDriverType4/testsuite_3.0/TestSimpleExecute.java
+++ libsrc/JDBCDriverType4/testsuite_3.0/TestSimpleExecute.java
@@ -1,25 +1,24 @@
 /*
- *  
  *  This file is part of the OpenLink Software Virtuoso Open-Source (VOS)
  *  project.
- *  
- *  Copyright (C) 1998-2006 OpenLink Software
- *  
+ *
+ *  Copyright (C) 1998-2009 OpenLink Software
+ *
  *  This project is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License as published by the
  *  Free Software Foundation; only version 2 of the License, dated June 1991.
- *  
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *  General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License along
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- *  
- *  
-*/
+ *
+ */
+
 package testsuite;
 
 import java.sql.*;
@@ -191,7 +190,7 @@ public class TestSimpleExecute
 	 try {
 	   stmt.executeUpdate ("drop table EX..DEMO");
 	 } catch (Exception e) { }
-         
+
          System.out.print("Execute CREATE TABLE");
          if(stmt.executeUpdate("create table EX..DEMO (ID integer,FILLER integer,primary key(ID))") == 0)
             System.out.println("    PASSED");
