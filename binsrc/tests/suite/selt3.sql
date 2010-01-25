8ae1dffb664e0d96b64fdc4119b4e25174c945a8
VOS Maintainer <vos.admin@openlinksw.com>
2010-01-25 23:28:52 +0000

    * Fixed copyright and license clarification

--- binsrc/tests/suite/selt3.sql
+++ binsrc/tests/suite/selt3.sql
@@ -26,17 +26,6 @@
 --  
 --  
 
---  (C)Copyright 2005 OpenLink Software.
---  All Rights Reserved.
---
---  The copyright above and this notice must be preserved in all
---  copies of this source code.  The copyright above does not
---  evidence any actual or intended publication of this source code.
---
---  This is unpublished proprietary trade secret of OpenLink Software.
---  This source code may not be copied, disclosed, distributed, demonstrated
---  or licensed except as authorized by OpenLink Software.
---
 set autocommit manual;
 select * from t1 order by fi2;
 commit work;
