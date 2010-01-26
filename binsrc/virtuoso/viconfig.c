28e149da8eb784d209abd3dcce994c26727e700a
VOS Maintainer <vos.admin@openlinksw.com>
2010-01-26 11:16:08 +0000

    * Added option BuffersAllocation

--- binsrc/virtuoso/viconfig.c
+++ binsrc/virtuoso/viconfig.c
@@ -885,6 +885,9 @@ cfg_setup (void)
   if (cfg_getlong (pconfig, section, "AsyncQueueMaxThreads", &c_aq_max_threads) == -1)
     c_aq_max_threads = 10;
 
+  if (cfg_getlong (pconfig, section, "BuffersAllocation", &malloc_bufs) == -1)
+    malloc_bufs = 0;
+
   {
     int nbdirs;
     dk_set_t bd = NULL;
