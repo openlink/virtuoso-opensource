40d4bc18b3e49e6d2a76788440cc40949ffee302
VOS Maintainer <vos.admin@openlinksw.com>
2010-01-27 12:51:34 +0000

    * Fixed do not POST when no post parameters

--- libsrc/Wi/virtual_dir.sql
+++ libsrc/Wi/virtual_dir.sql
@@ -1161,6 +1161,8 @@ end_loop:;
       body := rtrim (body, '&');
     else
       body := null;
+    if (body is null and meth = 'POST')
+      meth := 'GET';
     if (req_hdr is null)
       {
 	req_hdr := '';
