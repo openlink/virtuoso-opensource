005f71cba9c187ff2051a8c9cea5eead63f67693
VOS Maintainer <vos.admin@openlinksw.com>
2010-02-09 17:37:42 +0000

    * Fixed check to include iri_to_id

--- libsrc/Wi/sqlcost.c
+++ libsrc/Wi/sqlcost.c
@@ -883,7 +883,7 @@ sqlo_iri_constant_name (ST* tree)
     return (caddr_t)tree;
   if (ST_P (tree, CALL_STMT) && 1 <= BOX_ELEMENTS (tree->_.call.params)
       && DV_STRINGP (tree->_.call.name)
-      && 0 == strnicmp (tree->_.call.name, "__I2ID", 6)
+      && (0 == strnicmp (tree->_.call.name, "__I2ID", 6) || 0 == strnicmp (tree->_.call.name, "IRI_TO_ID", 9))
       && DV_STRINGP ((name = sqlo_iri_constant_name_1 (tree->_.call.params[0]))))
     return name;
   return NULL;
