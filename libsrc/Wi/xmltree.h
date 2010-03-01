48b8360f99bf37a4195bc5d4a265cfe42bd43fa8
VOS Maintainer <vos.admin@openlinksw.com>
2010-03-01 01:03:04 +0000

    * Added immortal IRI for uname_virtrdf_ns_uri_DefaultServiceStorage

--- libsrc/Wi/xmltree.h
+++ libsrc/Wi/xmltree.h
@@ -1065,6 +1065,7 @@ extern caddr_t uname_swap_reify_ns_uri;
 extern caddr_t uname_swap_reify_ns_uri_statement;
 extern caddr_t uname_virtrdf_ns_uri;
 extern caddr_t uname_virtrdf_ns_uri_DefaultQuadStorage;
+extern caddr_t uname_virtrdf_ns_uri_DefaultServiceStorage;
 extern caddr_t uname_virtrdf_ns_uri_PrivateGraphs;
 extern caddr_t uname_virtrdf_ns_uri_QuadMap;
 extern caddr_t uname_virtrdf_ns_uri_QuadMapFormat;
