6ecac0fe709267ebf3c5d1c97936341c76874d8b
VOS Maintainer <vos.admin@openlinksw.com>
2010-06-24 15:34:21 +0000

    * Added more properties for inference

--- binsrc/b3s/ns.sql
+++ binsrc/b3s/ns.sql
@@ -61,6 +61,8 @@ foaf:mbox_sha1sum a owl:InverseFunctionalProperty .
 foaf:name a owl:InverseFunctionalProperty .
 foaf:name rdfs:subPropertyOf lod:ifp_like .
 foaf:mbox_sha1sum rdfs:subPropertyOf lod:ifp_like .
+<http://linkedopencommerce.com/schemas/icecat/v1/hasProductId> a owl:InverseFunctionalProperty .
+<http://linkedopencommerce.com/schemas/icecat/v1/hasProductId> rdfs:subPropertyOf lod:ifp_like .
 
 ', 'xx', 'b3sifp');
 
