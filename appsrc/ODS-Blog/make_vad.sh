47c8bd59d8516ffe46788dcdb34f25ee54c953bc
VOS Maintainer <vos.admin@openlinksw.com>
2010-09-21 11:33:14 +0000

    * Added new templates

--- appsrc/ODS-Blog/make_vad.sh
+++ appsrc/ODS-Blog/make_vad.sh
@@ -228,6 +228,8 @@ directory_init() {
   mkdir vad/data/blog2/templates/thin_pastel
   mkdir vad/data/blog2/templates/squeaky_clean
   mkdir vad/data/blog2/templates/columns_clean
+  mkdir vad/data/blog2/templates/digital_bazaar
+  mkdir vad/data/blog2/templates/newspaper_view
   mkdir vad/data/blog2/widgets
   cp index.vspx vad/data/blog2
   cp *.sql vad/data/blog2
@@ -251,6 +253,9 @@ directory_init() {
   cp -f templates/thin_pastel/* vad/data/blog2/templates/thin_pastel 2>/dev/null
   cp -f templates/squeaky_clean/* vad/data/blog2/templates/squeaky_clean 2>/dev/null
   cp -f templates/columns_clean/* vad/data/blog2/templates/columns_clean 2>/dev/null
+  cp -f templates/digital_bazaar/* vad/data/blog2/templates/digital_bazaar 2>/dev/null
+  cp -f templates/newspaper_view/* vad/data/blog2/templates/newspaper_view 2>/dev/null
+
   cp -f templates/main.vspx vad/data/blog2/templates/main.vspx
   cp -rf widgets/* vad/data/blog2/widgets
 }
