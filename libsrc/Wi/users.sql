f18c76511dc5ff109a156b10a1bf7eb5447a12e8
VOS Maintainer <vos.admin@openlinksw.com>
2011-04-26 12:13:16 +0000

    * Fixed check webid only if not uathenticated

--- libsrc/Wi/users.sql
+++ libsrc/Wi/users.sql
@@ -1070,7 +1070,7 @@ normal_auth:
     {
       rc := "DB"."DBA"."DBEV_LOGIN" (user_name, digest, session_random);
     }
-  else
+  else if (rc <= 0) -- only if not authenticated
     {
       rc := DB.DBA.FOAF_SSL_LOGIN (user_name, digest, session_random);
       if (rc = 0)
