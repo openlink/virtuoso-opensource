90d23408cd21de0feb2697974148a3c06125c283
VOS Maintainer <vos.admin@openlinksw.com>
2011-06-02 12:34:40 +0000

    * Fixed issue with validation * Fixed WebDAV selection

--- appsrc/ODS-Bookmark/www/js/bookmark.js
+++ appsrc/ODS-Bookmark/www/js/bookmark.js
@@ -746,7 +746,10 @@ function readBookmark (id)
 
 function davBrowse (fld)
 {
-  var options = {mode: 'browser', onConfirmClick: function(path, fname) {$(fld).value = path + fname;}};
+  var options = {
+    mode: 'browser',
+    onConfirmClick: function(path, fname) {$(fld).value = '/DAV' + path + fname;}
+  };
   OAT.WebDav.open(options);
 }
 
