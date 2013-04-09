c30b6c6795f0fe00e97ab37c9bc4050d407c9816
VOS Maintainer <vos.admin@openlinksw.com>
2013-04-09 12:41:09 +0200

    Added alias MaxClientConnections for ServerThreads

--- binsrc/virtuoso/viconfig.c
+++ binsrc/virtuoso/viconfig.c
@@ -772,6 +772,7 @@ cfg_setup (void)
 #endif
 
   if (cfg_getlong (pconfig, section, "ServerThreads", &c_server_threads) == -1)
+    if (cfg_getlong (pconfig, section, "MaxClientConnections", &c_server_threads) == -1)
     c_server_threads = 10;
 
   if (cfg_getlong (pconfig, section, "CheckpointInterval", &c_checkpoint_interval) == -1)
@@ -1297,6 +1298,7 @@ cfg_setup (void)
 #endif
 
   if (cfg_getlong (pconfig, section, "ServerThreads", &c_http_threads) == -1)
+    if (cfg_getlong (pconfig, section, "MaxClientConnections", &c_http_threads) == -1)
     c_http_threads = 0;
 
   if (c_http_threads < 1 && c_http_port)
