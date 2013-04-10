9af97f7d27fa93aee24054f83790c1058c02c47a
VOS Maintainer <vos.admin@openlinksw.com>
2013-04-10 04:50:49 +0200

    Added missing var decl

--- libsrc/Wi/sqlsrv.c
+++ libsrc/Wi/sqlsrv.c
@@ -3399,6 +3399,7 @@ srv_compatibility_init (void)
 }
 
 long srv_pid = 0;
+long srv_cpu_count = 4;
 
 
 void
@@ -3743,6 +3744,7 @@ srv_session_disconnect_action (dk_session_t *ses)
 }
 
 void   rdf_key_comp_init ();
+extern int enable_col_by_default, c_col_by_default;
 long get_total_sys_mem ();
 
 void
