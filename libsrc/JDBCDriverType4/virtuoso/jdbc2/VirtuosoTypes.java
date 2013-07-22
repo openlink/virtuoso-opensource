fcd73cebaf54c92b2c51cc790cad5ee9a4852246
VOS Maintainer <vos.admin@openlinksw.com>
2013-07-22 10:47:05 +0200

    Fixed backward compatibility version

--- libsrc/JDBCDriverType4/virtuoso/jdbc2/VirtuosoTypes.java
+++ libsrc/JDBCDriverType4/virtuoso/jdbc2/VirtuosoTypes.java
@@ -35,7 +35,7 @@ class VirtuosoTypes
    /**
     * The version string.
     */
-   public static final String version = "06.00.3039";
+   public static final String version = "05.12.3039";
 
    /**
     * NIL, false, the NULL pointer
