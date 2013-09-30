76691a6e3fa8a62e0a3af5c8e0b1a8a8c5d76ca9
VOS Maintainer <vos.admin@openlinksw.com>
2013-09-30 11:39:36 +0200

    Fixed FCT VAD version

--- binsrc/b3s/make_vad.sh
+++ binsrc/b3s/make_vad.sh
@@ -93,26 +93,16 @@ else
   myrm=rm
 fi
 
+
 version_init()
 {
-  if [ $VOS -eq 1 ]
-  then
-      if [ -f vad_version ]
-      then
-      VERSION=`cat vad_version`
-      else
-        LOG "The vad_version does not exist, please verify your checkout"
-    exit 1
-      fi
-  else
-      rm -f version.tmp
-      for i in `find . -name 'Entries' | grep -v "vad/"`; do
-      cat $i | grep "^[^D].*" | cut -f 3 -d "/" | sed -e "s/1\.//g" >> version.tmp
-      done
-      VERSION=`cat version.tmp | awk ' BEGIN { cnt=9 } { cnt = cnt + $1 } END { printf "1.%02.02f", cnt/100 }'`
-      rm -f version.tmp
-      echo "$VERSION" > vad_version
-  fi
+    if [ -f vad_version ]
+    then
+	VERSION=`cat vad_version`
+    else
+	LOG "The vad_version does not exist, please verify your checkout"
+	exit 1
+    fi
 }
 
 do_command_safe () {
