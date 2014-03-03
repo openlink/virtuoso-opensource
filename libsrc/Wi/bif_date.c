4df2219583e672e6e66c7d90cdf5e2ee8a77ee89
VOS Maintainer <vos.admin@openlinksw.com>
2014-03-03 16:20:53 +0100

    Added ODBC 3.x alias for current_date, current_time and current_timestamp

--- libsrc/Wi/bif_date.c
+++ libsrc/Wi/bif_date.c
@@ -1000,8 +1000,11 @@ bif_date_init ()
   bif_define_ex ("merge_nasa_tjd_to_datetime"	, bif_merge_nasa_tjd_to_datetime	, BMD_RET_TYPE, &bt_datetime	, BMD_IS_PURE, BMD_DONE);
   bif_define_ex ("now"				, bif_timestamp				, BMD_RET_TYPE, &bt_timestamp	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard name */
   bif_define_ex ("getdate"			, bif_timestamp				, BMD_RET_TYPE, &bt_datetime	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard name? */
+  bif_define_ex ("current_timestamp"		, bif_timestamp				, BMD_RET_TYPE, &bt_datetime	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard name? */
   bif_define_ex ("curdate"			, bif_curdate				, BMD_RET_TYPE, &bt_date	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard fun. */
+  bif_define_ex ("current_date" 		, bif_curdate				, BMD_RET_TYPE, &bt_date	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard fun. */
   bif_define_ex ("curtime"			, bif_curtime				, BMD_RET_TYPE, &bt_time	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard fun. */
+  bif_define_ex ("current_time" 		, bif_curtime				, BMD_RET_TYPE, &bt_time	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is standard fun. */
   bif_define_ex ("curdatetime"			, bif_curdatetime			, BMD_RET_TYPE, &bt_timestamp	, /*BMD_IS_PURE,*/ BMD_DONE);	/* This is our own. */
   bif_define_ex ("datestring"			, bif_date_string			, BMD_RET_TYPE, &bt_varchar	, BMD_IS_PURE, BMD_DONE);
   bif_define_ex ("datestring_GMT"		, bif_date_string_GMT			, BMD_RET_TYPE, &bt_varchar	, BMD_IS_PURE, BMD_DONE);
