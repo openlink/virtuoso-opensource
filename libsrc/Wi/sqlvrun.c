30a70a7447d6c895af25ea420ab4a9e077c5fabd
VOS Maintainer <vos.admin@openlinksw.com>
2014-03-14 16:11:52 +0100

    Fixed bounds check
    Need safety check for more ways than sets because inc branch count
    returns aberrant numbers in cases, e.g.  if previous thread count
    negative

--- libsrc/Wi/sqlvrun.c
+++ libsrc/Wi/sqlvrun.c
@@ -2902,6 +2902,8 @@ ts_split_sets (table_source_t * ts, caddr_t * inst, it_cursor_t * itc, int n_par
   n_ways = 1 + qi_inc_branch_count (qi, enable_qp, n_ways - 1);
   if (n_ways < 2)
     return itc_reset (itc);
+  if (n_ways > n_sets)
+    n_ways = n_sets;
   chunk = n_sets / n_ways;
   for (inx = 0; inx < n_ways - 1; inx++)
     {
