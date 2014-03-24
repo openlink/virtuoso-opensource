578f12bacb3632b3d7e3670c79a2028d43413f99
VOS Maintainer <vos.admin@openlinksw.com>
2014-03-24 09:40:47 +0100

    Added missing grant

--- binsrc/b3s/grants.sql
+++ binsrc/b3s/grants.sql
@@ -59,3 +59,4 @@ grant execute on DB.DBA.lp_score_ck to "SPARQL_SELECT";
 grant execute on DB.DBA.IRI_RANK to "SPARQL_SELECT";
 grant execute on DB.DBA.RL_I2ID to "SPARQL_SELECT";
 grant execute on DB.DBA.RL_I2ID_NP to "SPARQL_SELECT";
+grant execute on DB.DBA.VT_BATCH_REAL_PROCESS_1_DB_DBA_RDF_OBJ to "SPARQL_SPONGE";
