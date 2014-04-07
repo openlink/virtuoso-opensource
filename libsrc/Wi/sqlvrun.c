32868d8d22e6084bceec4507e22307edb7e1802b
VOS Maintainer <vos.admin@openlinksw.com>
2014-04-07 12:21:00 +0200

    fixed if scalar param in itc, set the vec place for it to null in copying

--- libsrc/Wi/sqlvrun.c
+++ libsrc/Wi/sqlvrun.c
@@ -2361,6 +2361,7 @@ itc_copy (it_cursor_t * itc)
       caddr_t c = NULL;
       if (!itc->itc_n_sets || !ITC_P_VEC (itc, inx))
 	c = box_mt_copy_tree (itc->itc_search_params[inx]);
+      ITC_P_VEC (cp, inx) = NULL;
       ITC_SEARCH_PARAM (cp, c);
       if (c)
 	ITC_OWNS_PARAM (cp, c);
