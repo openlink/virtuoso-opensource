50a1a06fba485d8d9adc002a4d458d5fbcb8fe0f
VOS Maintainer <vos.admin@openlinksw.com>
2014-05-20 12:32:08 +0200

    Fixed moved code

--- libsrc/Wi/clsql.c
+++ libsrc/Wi/clsql.c
@@ -1497,6 +1497,17 @@ sqlg_cl_multistate_group (sql_comp_t * sc)
   END_DO_SET ();
 }
 
+int enable_high_card_part = 0;
+float c_setp_partition_threshold = 100000;
+
+int
+setp_is_high_card (setp_node_t * setp)
+{
+  return enable_high_card_part && setp->setp_card > c_setp_partition_threshold && !setp->setp_in_union;
+}
+
+
+
 void
 qf_set_max_rows (query_frag_t * qf, int max)
 {
@@ -1593,14 +1604,4 @@ int
 sqlg_distinct_colocated (sql_comp_t * sc, state_slot_t ** ssls, int n_ssls)
 {
   return 0;
-}
-
-
-int enable_high_card_part = 0;
-float c_setp_partition_threshold = 100000;
-
-int
-setp_is_high_card (setp_node_t * setp)
-{
-  return enable_high_card_part && setp->setp_card > c_setp_partition_threshold && !setp->setp_in_union;
 }
