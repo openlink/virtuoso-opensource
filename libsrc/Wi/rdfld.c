e5b9cf19c79581905e66f01d94d3bd479e849a8a
VOS Maintainer <vos.admin@openlinksw.com>
2014-09-24 15:55:09 +0200

    Fixed should use zero iri otherwise dc becomes heterogenious (part 2)

--- libsrc/Wi/rdfld.c
+++ libsrc/Wi/rdfld.c
@@ -703,7 +703,7 @@ l_iri_id_disp (cucurbit_t * cu, caddr_t name, value_state_t * vs)
     }
   {
     AUTO_POOL (8);
-    cu_local_dispatch (cu, vs, cf, (caddr_t) ap_list (&ap, 2, local, NULL));
+    cu_local_dispatch (cu, vs, cf, (caddr_t) ap_list (&ap, 2, local, ap_box_iri_id (&ap, 0)));
     dk_free_box (local);
   }
   return NULL;
