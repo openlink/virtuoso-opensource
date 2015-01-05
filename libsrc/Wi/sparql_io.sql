8ab59f2e12d1d78aa3c65fb77a9d7fc9552b9927
VOS Maintainer <vos.admin@openlinksw.com>
2015-01-05 18:52:47 +0100

    Added indicator when max rows is returned on /sparql endpopint

--- libsrc/Wi/sparql_io.sql
+++ libsrc/Wi/sparql_io.sql
@@ -3467,6 +3467,8 @@ host_found:
   start_time := msec_time();
   exec ( concat ('sparql {', full_query, '\n}'), state, msg, qry_params, vector ('max_rows', maxrows, 'use_cache', 1), metas, rset);
   commit work;
+  if (isvector (rset) and length (rset) = maxrows)
+    http_header (http_header_get () || sprintf ('X-SPARQL-MaxRows: %d\r\n', maxrows));
   -- dbg_obj_princ ('exec metas=', metas, ', state=', state, ', msg=', msg);
   if (state = '00000')
     goto write_results;
