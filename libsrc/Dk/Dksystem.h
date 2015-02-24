acc5a22de122d1c00d6ccea6522b46be59cad620
VOS Maintainer <vos.admin@openlinksw.com>
2015-02-24 12:22:12 +0100

    Commented out declspec for windows as it interferes with EXPORT

--- libsrc/Dk/Dksystem.h
+++ libsrc/Dk/Dksystem.h
@@ -191,7 +191,7 @@ char *strtok_r ();
  *  Portable way to tag functions that do not return
  */
 #if (_MSC_VER >= 1310)		/* MS Visual Studio 2003/.NET Framework 1.1 or newer */
-#  define NORETURN 		__declspec(noreturn)
+#  define NORETURN 		/*__declspec(noreturn)*/
 #elif defined(__HP_cc) && (__HP_cc >= 61000)
 #  define NORETURN 		__attribute__((noreturn))
 #elif (GCC_VERSION >= 3004) || defined (__clang__)
