ad0e04c06e7addbb0d215b6774ba3e9399fa0618
VOS Maintainer <vos.admin@openlinksw.com>
2015-05-04 17:23:56 +0200

    Updated version of the JDBC driver

--- libsrc/JDBCDriverType4/virtuoso/jdbc2/Driver.java
+++ libsrc/JDBCDriverType4/virtuoso/jdbc2/Driver.java
@@ -62,7 +62,7 @@ public class Driver implements java.sql.Driver
    // The major and minor version number
    protected static final int major = 3;
 
-   protected static final int minor = 77;
+   protected static final int minor = 78;
 
    // Some variables
    private String host = "localhost";
