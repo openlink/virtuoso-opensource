1234d4d2964e13041329d1e60bd62aa6fc2f9b5e
Sergey Malinin <smalinin@openlinksw.com>
2015-07-27 14:06:19 +0200

    Updated version of Jena2 and Sesame2 providers

--- binsrc/jena2/virtuoso_driver/Version.java
+++ binsrc/jena2/virtuoso_driver/Version.java
@@ -29,7 +29,7 @@ public class Version {
    protected static final String DRV_VER = "2.12.0";
 
    // The major and minor version number
-   protected static final String build_id = "1.20";
+   protected static final String build_id = "1.21";
 
    public static void main(String args[])
    {
