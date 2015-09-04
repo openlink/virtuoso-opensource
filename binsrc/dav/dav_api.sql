0c5b8292b0345bbe2c759259b3ee435485aaaf27
VOS Maintainer <vos.admin@openlinksw.com>
2015-09-04 11:30:36 +0200

    Fixed issues deleting permissions

--- binsrc/dav/dav_api.sql
+++ binsrc/dav/dav_api.sql
@@ -5243,18 +5243,30 @@ create procedure WS.WS.WAC_DELETE (
     connection_set ('dav_acl_sync', null);
   }
   set_user_id ('dba');
-  delete from DB.DBA.RDF_QUAD b where exists (select 1 from DB.DBA.RDF_QUAD a WHERE a.G = b.G and a.S = b.S and a.P = b.P and a.O = b.O and
-      	a.G = __i2idn (graph) and
-  	a.P = __i2idn ('http://www.w3.org/1999/02/22-rdf-syntax-ns#type') and
-  	a.O = __i2idn ('http://www.w3.org/ns/auth/acl#Authorization') );
-  delete from DB.DBA.RDF_QUAD b where exists (select 1 from DB.DBA.RDF_QUAD a WHERE a.G = b.G and a.S = b.S and a.P = b.P and a.O = b.O and
-      	a.G = __i2idn (graph) and
-  	a.P = __i2idn ('http://www.w3.org/1999/02/22-rdf-syntax-ns#type') and
-  	a.O = __i2idn ('http://www.openlinksw.com/schemas/acl/filter#Filter') );
-  delete from DB.DBA.RDF_QUAD b where exists (select 1 from DB.DBA.RDF_QUAD a WHERE a.G = b.G and a.S = b.S and a.P = b.P and a.O = b.O and
-      	a.G = __i2idn (graph) and
-  	a.P = __i2idn ('http://www.w3.org/1999/02/22-rdf-syntax-ns#type') and
-  	a.O = __i2idn ('http://www.openlinksw.com/schemas/acl/filter#Criteria') );
+  for (select a.G as GG, a.S as SS
+         from DB.DBA.RDF_QUAD a
+        where	a.G = __i2idn (graph)
+          and	a.P = __i2idn ('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+          and	a.O = __i2idn ('http://www.w3.org/ns/auth/acl#Authorization')) do
+	{
+	  delete from DB.DBA.RDF_QUAD where G = GG and S = SS or O = SS;
+	}
+  for (select a.G as GG, a.S as SS
+         from DB.DBA.RDF_QUAD a
+        where	a.G = __i2idn (graph)
+          and a.P = __i2idn ('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+          and a.O = __i2idn ('http://www.openlinksw.com/schemas/acl/filter#Filter')) do
+	{
+	  delete from DB.DBA.RDF_QUAD where G = GG and S = SS or O = SS;
+	}
+  for (select a.G as GG, a.S as SS
+         from DB.DBA.RDF_QUAD a
+        where	a.G = __i2idn (graph)
+          and a.P = __i2idn ('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+          and a.O = __i2idn ('http://www.openlinksw.com/schemas/acl/filter#Criteria')) do
+	{
+	  delete from DB.DBA.RDF_QUAD where G = GG and S = SS or O = SS;
+	}
 }
 ;
 
