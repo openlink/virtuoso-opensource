e58ed068356680d2ad9eee4ee084d54706ab2517
VOS Maintainer <vos.admin@openlinksw.com>
2015-10-14 12:43:24 +0200

    Fixed version of Jena 2.x provider

--- binsrc/jena2/virtuoso_driver/Version.java
+++ binsrc/jena2/virtuoso_driver/Version.java
@@ -29,7 +29,7 @@ public class Version {
    protected static final String DRV_VER = "2.12.0";
 
    // The major and minor version number
-   protected static final String build_id = "1.22";
+   protected static final String build_id = "1.23";
 
    public static void main(String args[])
    {
