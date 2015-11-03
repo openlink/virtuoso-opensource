079c239c6c5cb36d84d9fbd864c949d66a78ff69
VOS Maintainer <vos.admin@openlinksw.com>
2015-11-03 14:32:43 +0100

    Added checks for Virtuoso JDBC 4 and 4_1 providers

--- binsrc/tests/suite/tjdbc.sh
+++ binsrc/tests/suite/tjdbc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 #
 #  $Id: tjdbc.sh,v 1.6.6.1.4.4 2013/01/02 16:15:12 source Exp $
 #
@@ -35,82 +36,158 @@ BANNER "STARTED JDBC Driver TEST (tjdbc.sh)"
 
 if [ "x$JDK2" != "x" -a -f $JDBCDIR/virtjdbc2.jar -a -f  $JDBCDIR/testsuite2.jar ]
 then
+    STOP_SERVER
+    rm -f $DBLOGFILE
+    rm -f $DBFILE
+    MAKECFG_FILE $TESTCFGFILE $PORT $CFGFILE
 
-STOP_SERVER
-rm -f $DBLOGFILE
-rm -f $DBFILE
-MAKECFG_FILE $TESTCFGFILE $PORT $CFGFILE
+    START_SERVER $PORT 1000
 
-START_SERVER $PORT 1000
+    ECHO "STARTED: JDBC 2 Test suite"
+    cd $JDBCDIR
+    sh $JDBCDIR/test2.sh "jdbc:virtuoso://localhost:$PORT/" > jdbc.out 2>&1
+    cd $CURRDIR
 
-ECHO "STARTED: JDBC 2 Test suite"
-cd $JDBCDIR
-sh $JDBCDIR/test2.sh "jdbc:virtuoso://localhost:$PORT/" > jdbc.out 2>&1
-cd $CURRDIR
+    passed=`grep "PASSED" $JDBCDIR/jdbc.out`
+    passed_cnt=`grep "PASSED" $JDBCDIR/jdbc.out | wc -l`
+    failed=`grep "FAILED" $JDBCDIR/jdbc.out`
+    failed_cnt=`grep "FAILED" $JDBCDIR/jdbc.out | wc -l`
 
-passed=`grep "PASSED" $JDBCDIR/jdbc.out`
-passed_cnt=`grep "PASSED" $JDBCDIR/jdbc.out | wc -l`
-failed=`grep "FAILED" $JDBCDIR/jdbc.out`
-failed_cnt=`grep "FAILED" $JDBCDIR/jdbc.out | wc -l`
+    errors=0
+    if [ $failed_cnt -gt 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: Some JDBC 2 Tests failed (check $JDBCDIR/jdbc.out): $failed"
+    fi
+    if [ $passed_cnt -eq 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: no JDBC 2 Tests passed ! Check $JDBCDIR/jdbc.out"
+    fi
+    if [ $errors -eq 0 ]
+    then
+	ECHO "PASSED: JDBC 2 Test suite (check $JDBCDIR/jdbc.out)"
+    fi
 
-errors=0
-if [ $failed_cnt -gt 0 ]
-then
-errors=1
-ECHO "*** FAILED: Some JDBC 2 Tests failed (check $JDBCDIR/jdbc.out): $failed"
-fi
-if [ $passed_cnt -eq 0 ]
-then
-errors=1
-ECHO "*** FAILED: no JDBC 2 Tests passed ! Check $JDBCDIR/jdbc.out"
-fi
-if [ $errors -eq 0 ]
-then
-ECHO "PASSED: JDBC 2 Test suite (check $JDBCDIR/jdbc.out)"
-fi
-
-SHUTDOWN_SERVER
-CHECK_LOG
+    SHUTDOWN_SERVER
+    CHECK_LOG
 fi # JDK2
 
 if [ "x$JDK3" != "x" -a -f $JDBCDIR/virtjdbc3ssl.jar -a -f  $JDBCDIR/testsuite3.jar ]
 then
+    STOP_SERVER
+    rm -f $DBLOGFILE
+    rm -f $DBFILE
+    MAKECFG_FILE $TESTCFGFILE $PORT $CFGFILE
 
-STOP_SERVER
-rm -f $DBLOGFILE
-rm -f $DBFILE
-MAKECFG_FILE $TESTCFGFILE $PORT $CFGFILE
+    START_SERVER $PORT 1000
 
-START_SERVER $PORT 1000
+    ECHO "STARTED: JDBC 3 Test suite"
+    cd $JDBCDIR
+    sh $JDBCDIR/test3.sh "jdbc:virtuoso://localhost:$PORT/" > jdbc3.out 2>&1
+    cd $CURRDIR
 
-ECHO "STARTED: JDBC 3 Test suite"
-cd $JDBCDIR
-sh $JDBCDIR/test3.sh "jdbc:virtuoso://localhost:$PORT/" > jdbc3.out 2>&1
-cd $CURRDIR
+    passed=`grep "PASSED" $JDBCDIR/jdbc3.out`
+    passed_cnt=`grep "PASSED" $JDBCDIR/jdbc3.out | wc -l`
+    failed=`grep "FAILED" $JDBCDIR/jdbc3.out`
+    failed_cnt=`grep "FAILED" $JDBCDIR/jdbc3.out | wc -l`
 
-passed=`grep "PASSED" $JDBCDIR/jdbc3.out`
-passed_cnt=`grep "PASSED" $JDBCDIR/jdbc3.out | wc -l`
-failed=`grep "FAILED" $JDBCDIR/jdbc3.out`
-failed_cnt=`grep "FAILED" $JDBCDIR/jdbc3.out | wc -l`
+    errors=0
+    if [ $failed_cnt -gt 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: Some JDBC 3 Tests failed (check $JDBCDIR/jdbc3.out): $failed"
+    fi
+    if [ $passed_cnt -eq 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: no JDBC 3 Tests passed ! Check $JDBCDIR/jdbc3.out"
+    fi
+    if [ $errors -eq 0 ]
+    then
+	ECHO "PASSED: JDBC 3 Test suite (check $JDBCDIR/jdbc3.out)"
+    fi
 
-errors=0
-if [ $failed_cnt -gt 0 ]
-then
-errors=1
-ECHO "*** FAILED: Some JDBC 3 Tests failed (check $JDBCDIR/jdbc3.out): $failed"
-fi
-if [ $passed_cnt -eq 0 ]
-then
-errors=1
-ECHO "*** FAILED: no JDBC 3 Tests passed ! Check $JDBCDIR/jdbc3.out"
-fi
-if [ $errors -eq 0 ]
-then
-ECHO "PASSED: JDBC 3 Test suite (check $JDBCDIR/jdbc3.out)"
-fi
-
-SHUTDOWN_SERVER
-CHECK_LOG
+    SHUTDOWN_SERVER
+    CHECK_LOG
 fi # JDK3
+
+if [ "x$JDK4" != "x" -a -f $JDBCDIR/virtjdbc4ssl.jar -a -f  $JDBCDIR/testsuite4.jar ]
+then
+    STOP_SERVER
+    rm -f $DBLOGFILE
+    rm -f $DBFILE
+    MAKECFG_FILE $TESTCFGFILE $PORT $CFGFILE
+
+    START_SERVER $PORT 1000
+
+    ECHO "STARTED: JDBC 4 Test suite"
+    cd $JDBCDIR
+    sh $JDBCDIR/test4.sh "jdbc:virtuoso://localhost:$PORT/" > jdbc4.out 2>&1
+    cd $CURRDIR
+
+    passed=`grep "PASSED" $JDBCDIR/jdbc4.out`
+    passed_cnt=`grep "PASSED" $JDBCDIR/jdbc4.out | wc -l`
+    failed=`grep "FAILED" $JDBCDIR/jdbc4.out`
+    failed_cnt=`grep "FAILED" $JDBCDIR/jdbc4.out | wc -l`
+
+    errors=0
+    if [ $failed_cnt -gt 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: Some JDBC 4 Tests failed (check $JDBCDIR/jdbc4.out): $failed"
+    fi
+    if [ $passed_cnt -eq 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: no JDBC 4 Tests passed ! Check $JDBCDIR/jdbc4.out"
+    fi
+    if [ $errors -eq 0 ]
+    then
+	ECHO "PASSED: JDBC 4 Test suite (check $JDBCDIR/jdbc4.out)"
+    fi
+
+    SHUTDOWN_SERVER
+    CHECK_LOG
+fi # JDK4
+
+if [ "x$JDK4_1" != "x" -a -f $JDBCDIR/virtjdbc4_1ssl.jar -a -f  $JDBCDIR/testsuite4.jar ]
+then
+    STOP_SERVER
+    rm -f $DBLOGFILE
+    rm -f $DBFILE
+    MAKECFG_FILE $TESTCFGFILE $PORT $CFGFILE
+
+    START_SERVER $PORT 1000
+
+    ECHO "STARTED: JDBC 4_1 Test suite"
+    cd $JDBCDIR
+    sh $JDBCDIR/test4_1.sh "jdbc:virtuoso://localhost:$PORT/" > jdbc4_1.out 2>&1
+    cd $CURRDIR
+
+    passed=`grep "PASSED" $JDBCDIR/jdbc4_1.out`
+    passed_cnt=`grep "PASSED" $JDBCDIR/jdbc4_1.out | wc -l`
+    failed=`grep "FAILED" $JDBCDIR/jdbc4_1.out`
+    failed_cnt=`grep "FAILED" $JDBCDIR/jdbc4_1.out | wc -l`
+
+    errors=0
+    if [ $failed_cnt -gt 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: Some JDBC 4_1 Tests failed (check $JDBCDIR/jdbc4_1.out): $failed"
+    fi
+    if [ $passed_cnt -eq 0 ]
+    then
+	errors=1
+	ECHO "*** FAILED: no JDBC 4_1 Tests passed ! Check $JDBCDIR/jdbc4_1.out"
+    fi
+    if [ $errors -eq 0 ]
+    then
+	ECHO "PASSED: JDBC 4_1 Test suite (check $JDBCDIR/jdbc4_1.out)"
+    fi
+
+    SHUTDOWN_SERVER
+    CHECK_LOG
+fi # JDK4_1
 
 BANNER "COMPLETED JDBC Driver TEST (tjdbc.sh)"
