d0bad521232d21d1abf8f22148549e0b4ae63c53
Sergey Malinin <smalinin@openlinksw.com>
2016-06-28 18:40:33 +1000

    RDF4J provider for Virtuoso

---
+++ binsrc/rdf4j/virtuoso_driver/VirtuosoTupleQuery.java
@@ -0,0 +1,35 @@
+/*
+ *  $Id$
+ *
+ *  This file is part of the OpenLink Software Virtuoso Open-Source (VOS)
+ *  project.
+ *
+ *  Copyright (C) 1998-2016 OpenLink Software
+ *
+ *  This project is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; only version 2 of the License, dated June 1991.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+package virtuoso.rdf4j.driver;
+
+import org.eclipse.rdf4j.query.*;
+
+public class VirtuosoTupleQuery extends VirtuosoQuery  implements TupleQuery {
+    public TupleQueryResult evaluate() throws QueryEvaluationException {
+        return null;
+    }
+
+    public void evaluate(TupleQueryResultHandler tupleQueryResultHandler) throws QueryEvaluationException, TupleQueryResultHandlerException {
+
+    }
+}
