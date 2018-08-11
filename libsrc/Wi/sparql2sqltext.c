a5899d26ad691dda5d77ce2dc28dfe1a590196bf
Patrick van Kleef <pkleef@openlinksw.com>
2018-08-11 23:22:44 +0200

    Fixed issue with cast numeric to bool

--- libsrc/Wi/sparql2sqltext.c
+++ libsrc/Wi/sparql2sqltext.c
@@ -5424,7 +5424,7 @@ ssg_print_bool_of_plain_box (spar_sqlgen_t *ssg, caddr_t val)
     case DV_LONG_INT: ssg_puts (unbox (val) ? " 1" : " 0"); ssg_puts_comment ("int as bool"); break;
     case DV_SINGLE_FLOAT:  ssg_puts (unbox_float (val) ? " 1" : " 0"); ssg_puts_comment ("real as bool"); break;
     case DV_DOUBLE_FLOAT:  ssg_puts (unbox_double (val) ? " 1" : " 0"); ssg_puts_comment ("double as bool"); break;
-    case DV_NUMERIC:  ssg_puts (num_is_zero ((numeric_t)(val)) ? " 1" : " 0"); ssg_puts_comment ("decimal as bool"); break;
+    case DV_NUMERIC:  ssg_puts (num_is_zero ((numeric_t)(val)) ? " 0" /* not 1! */ : " 1" /* not 0! */); ssg_puts_comment ("decimal as bool"); break;
     case DV_DATETIME: case DV_XML_ENTITY: case DV_GEO: ssg_puts (" 1"); ssg_puts_comment ("atom as bool"); break;
     case DV_STRING: case DV_BIN: ssg_puts ((1 < box_length (val)) ? " 1" : " 0"); ssg_puts_comment ("string as bool"); break;
     case DV_WIDE:  ssg_puts ((sizeof(wchar_t) < box_length (val)) ? " 1" : " 0"); ssg_puts_comment ("wide as bool"); break;
