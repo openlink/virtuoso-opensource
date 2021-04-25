5b7dd5c9b263450b26e9672f15c0858c35cfca51
Patrick van Kleef <pkleef@openlinksw.com>
2021-04-25 11:33:14 +0200

    Updated driver version

--- libsrc/JDBCDriverType4/virtuoso/jdbc/Driver.java
+++ libsrc/JDBCDriverType4/virtuoso/jdbc/Driver.java
@@ -62,7 +62,7 @@ public class Driver implements java.sql.Driver
    // The major and minor version number
    protected static final int major = 3;
 
-   protected static final int minor = 117;
+   protected static final int minor = 118;
 
    // Some variables
    private String host = "localhost";
