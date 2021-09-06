69f1a3e7432a5b565f866a564858e2204f14cade
Mitko Iliev <imitko@openlinksw.com>
2021-09-06 23:31:25 +0200

    Added RDF_DUMP_GRAPH and RDF_DUMP_NQUADS as built-in stored procedures

--- libsrc/Wi/system2.sql
+++ libsrc/Wi/system2.sql
@@ -2105,3 +2105,110 @@ create procedure dpipe_drop (in n varchar)
 }
 ;
 
+
+
+create procedure RDF_DUMP_GRAPH
+  (IN  srcgraph           VARCHAR,
+   IN  out_file           VARCHAR,
+   IN  file_length_limit  INTEGER  := 1000000000)
+{
+  DECLARE  file_name VARCHAR;
+  DECLARE  env, ses ANY;
+  DECLARE  ses_len, max_ses_len, file_len, file_idx INTEGER;
+
+   SET ISOLATION = 'uncommitted';
+   max_ses_len  := 10000000;
+   file_len     := 0;
+   file_idx     := 1;
+   file_name    := sprintf ('%s%06d.ttl', out_file, file_idx);
+   string_to_file (file_name || '.graph', srcgraph, -2);
+   string_to_file (file_name, sprintf ( '# Dump of graph <%s>, as of %s\n@base <> .\n', srcgraph, CAST (NOW() AS VARCHAR)), -2);
+   env := vector (dict_new (16000), 0, '', '', '', 0, 0, 0, 0, 0);
+   ses := string_output ();
+   FOR (SELECT * FROM ( SPARQL DEFINE input:storage "" SELECT ?s ?p ?o { GRAPH `iri(?:srcgraph)` { ?s ?p ?o } } ) AS sub OPTION (LOOP)) DO
+      {
+        http_ttl_triple (env, "s", "p", "o", ses);
+        ses_len := length (ses);
+        IF (ses_len > max_ses_len)
+          {
+            file_len := file_len + ses_len;
+            IF (file_len > file_length_limit)
+              {
+                http (' .\n', ses);
+                string_to_file (file_name, ses, -1);
+		gz_compress_file (file_name, file_name||'.gz');
+		file_delete (file_name);
+                file_len := 0;
+                file_idx := file_idx + 1;
+                file_name := sprintf ('%s%06d.ttl', out_file, file_idx);
+                string_to_file (file_name, sprintf ( '# Dump of graph <%s>, as of %s (part %d)\n@base <> .\n',
+		      srcgraph, CAST (NOW() AS VARCHAR), file_idx), -2);
+                 env := VECTOR (dict_new (16000), 0, '', '', '', 0, 0, 0, 0, 0);
+              }
+            ELSE
+              string_to_file (file_name, ses, -1);
+            ses := string_output ();
+          }
+      }
+    IF (LENGTH (ses))
+      {
+        http (' .\n', ses);
+        string_to_file (file_name, ses, -1);
+	gz_compress_file (file_name, file_name||'.gz');
+	file_delete (file_name);
+      }
+}
+;
+
+create procedure RDF_DUMP_NQUADS
+   (IN  dir                VARCHAR := 'dumps',
+    IN  start_from         INT := 1,
+    IN  file_length_limit  INTEGER := 100000000,
+    IN  comp               INT := 1)
+{
+  DECLARE  inx, ses_len  INT;
+  DECLARE  file_name     VARCHAR;
+  DECLARE  env, ses      ANY;
+
+
+  inx := start_from;
+  SET isolation = 'uncommitted';
+  env := vector (0,0,0);
+  ses := string_output (10000000);
+  FOR (SELECT * FROM (sparql define input:storage "" SELECT ?s ?p ?o ?g { GRAPH ?g { ?s ?p ?o } . FILTER ( ?g != virtrdf: ) } ) AS sub OPTION (loop)) DO
+    {
+      DECLARE EXIT HANDLER FOR SQLSTATE '22023'
+	{
+	  GOTO next;
+	};
+      http_nquad (env, "s", "p", "o", "g", ses);
+      ses_len := LENGTH (ses);
+      IF (ses_len >= file_length_limit)
+	{
+	  file_name := sprintf ('%s/rdf-dump-%06d.nq', dir, inx);
+	  string_to_file (file_name, ses, -2);
+	  IF (comp)
+	    {
+	      gz_compress_file (file_name, file_name||'.gz');
+	      file_delete (file_name);
+	    }
+	  inx := inx + 1;
+	  env := vector (0,0,0);
+	  ses := string_output (10000000);
+	}
+      next:;
+    }
+  IF (length (ses))
+    {
+      file_name := sprintf ('%s/output%06d.nq', dir, inx);
+      string_to_file (file_name, ses, -2);
+      IF (comp)
+	{
+	  gz_compress_file (file_name, file_name||'.gz');
+	  file_delete (file_name);
+	}
+      inx := inx + 1;
+      env := vector (0,0,0);
+    }
+}
+;
