357d021429b1073e4d26b02007634e374f3e9d7a
Patrick van Kleef <pkleef@openlinksw.com>
2022-03-29 19:48:09 +0200

    Fixed name of arg

--- libsrc/Wi/xslt.c
+++ libsrc/Wi/xslt.c
@@ -3650,7 +3650,7 @@ dict_put_impl (id_hash_iterator_t *hit, caddr_t key, caddr_t val, int signal_uns
 skip_insertion:
   HT_UNLOCK_COND(ht,wrlocked);
   res = ht->ht_inserts - ht->ht_deletes;
-  if (signal_unsafe_or_wrong_args && ht->ht_dict_max_mem_in_use > 0
+  if (signal_unsafe_args && ht->ht_dict_max_mem_in_use > 0
       && ht->ht_mp != NULL && ((mem_pool_t *)ht->ht_mp)->mp_bytes > ht->ht_dict_max_mem_in_use)
     sqlr_new_error ("42000", "D1CT0", "Hash dictionary memory pool is full, %ld exceeded %ld bytes",
         ((mem_pool_t *)ht->ht_mp)->mp_bytes, ht->ht_dict_max_mem_in_use);
