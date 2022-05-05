46a141f5bab424c984ae9da569af1a0167a14918
Patrick van Kleef <pkleef@openlinksw.com>
2022-05-05 11:36:43 +0200

    Updated version of Jena and RDF4j providers

--- binsrc/jena4/virtuoso_driver/Version.java
+++ binsrc/jena4/virtuoso_driver/Version.java
@@ -29,7 +29,7 @@ public class Version {
    protected static final String DRV_VER = "4.4.0";
 
    // The major and minor version number
-   protected static final String build_id = "1.37";
+   protected static final String build_id = "1.38";
 
    public static void main(String args[])
    {
