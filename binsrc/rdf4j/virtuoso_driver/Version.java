df1cf9f0e22afe4625243f71227557039df60980
Sergey Malinin <smalinin@openlinksw.com>
2022-07-06 18:04:41 +0300

    Fix issue with insert Literal with Language
    https://github.com/openlink/virtuoso-opensource/issues/1056

--- binsrc/rdf4j/virtuoso_driver/Version.java
+++ binsrc/rdf4j/virtuoso_driver/Version.java
@@ -26,7 +26,7 @@ public class Version {
     protected static final String DRV_VER = "3.7.6";
 
     // The major and minor version number
-    protected static final String build_id = "1.14";
+    protected static final String build_id = "1.15";
 
     public static void main(String args[])
     {
