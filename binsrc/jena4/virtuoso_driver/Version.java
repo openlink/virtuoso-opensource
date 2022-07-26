be8cd1bc7291ae4d2e2ab532fc99bb6d667227d1
Sergey Malinin <smalinin@openlinksw.com>
2022-07-26 17:29:25 +0300

    Update build_id

--- binsrc/jena4/virtuoso_driver/Version.java
+++ binsrc/jena4/virtuoso_driver/Version.java
@@ -29,7 +29,7 @@ public class Version {
    protected static final String DRV_VER = "4.4.0";
 
    // The major and minor version number
-   protected static final String build_id = "1.41";
+   protected static final String build_id = "1.42";
 
    public static void main(String args[])
    {
