8aba3e4e997fffc8a889bf884994b68e1f394aa5
Patrick van Kleef <pkleef@openlinksw.com>
2022-10-07 18:22:19 +0200

    Updated version of RDF4j v4 provider

--- binsrc/rdf4j_4/virtuoso_driver/Version.java
+++ binsrc/rdf4j_4/virtuoso_driver/Version.java
@@ -26,7 +26,7 @@ public class Version {
     protected static final String DRV_VER = "4.0.2";
 
     // The major and minor version number
-    protected static final String build_id = "1.16";
+    protected static final String build_id = "1.17";
 
     public static void main(String args[])
     {
