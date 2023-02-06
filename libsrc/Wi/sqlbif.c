f687eef090f81914dc108cde10dfb4f74717a850
Mitko Iliev <imitko@openlinksw.com>
2023-02-07 00:02:05 +0100

    Added support to calc length of DV_DICT_ITERATOR

--- libsrc/Wi/sqlbif.c
+++ libsrc/Wi/sqlbif.c
@@ -2216,6 +2216,12 @@ bif_length (caddr_t * qst, caddr_t * err_ret, state_slot_t ** args)
     else
   sqlr_new_error ("22023", "SR015", "Function length is not applicable to XML tree entity");
 #endif
+    case DV_DICT_ITERATOR:
+      {
+	id_hash_iterator_t *hit = bif_dict_iterator_arg (qst, args, 0, "length", 0);
+	id_hash_t *ht = hit->hit_hash;
+	return box_num (ht->ht_inserts - ht->ht_deletes);
+      }
     }
   if (IS_BOX_POINTER (arg))
   len = box_length (arg);
