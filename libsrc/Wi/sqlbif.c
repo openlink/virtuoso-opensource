9c1e9710e7d98a4b584e220e0d33691c5806f8cf
Mitko Iliev <imitko@openlinksw.com>
2023-02-06 18:12:21 +0100

    Fixed missing BIF alias

--- libsrc/Wi/sqlbif.c
+++ libsrc/Wi/sqlbif.c
@@ -17357,7 +17357,7 @@ sql_bif_init (void)
   bif_define ("__set_identity", bif_set_identity);
   bif_define_ex ("set_user_id", bif_set_user_id, BMD_ALIAS, "__set_user_id", BMD_DONE);
   bif_define ("get_user_id", bif_get_user_id);
-  bif_define ("get_user_id_by_name", bif_get_user_id_by_name);
+  bif_define_ex ("get_user_id_by_name", bif_get_user_id_by_name, BMD_ALIAS, "user_to_uid", BMD_RET_TYPE, &bt_integer, BMD_DONE);
   bif_define ("__pop_user_id", bif_pop_user_id);
   bif_define ("identity_value", bif_identity_value);
   fcache_init ();
