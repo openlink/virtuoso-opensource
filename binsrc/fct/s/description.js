b00bfdcea1b93b34860d46625fb92ff07a39008e
Patrick van Kleef <pkleef@openlinksw.com>
2023-05-01 11:07:02 +0200

    Removed merge artifact (fixes #1144)

--- binsrc/fct/s/description.js
+++ binsrc/fct/s/description.js
@@ -20,7 +20,8 @@
  *  51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  */
-var $j = jQuery.noConflict();
+
+//var $j = jQuery.noConflict();
 
 function init() {
         LoadExternalImages();
