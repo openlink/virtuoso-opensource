2afdcf92ee831fcbdf1d72fc72481754ddecc4b7
Patrick van Kleef <pkleef@openlinksw.com>
2025-12-12 14:48:23 +0000

    Fixed missing cast for argument 1 of aio_suspend
    Fixes #1367

--- libsrc/Wi/mtwrite.c
+++ libsrc/Wi/mtwrite.c
@@ -622,7 +622,7 @@ iq_aio (io_queue_t * iq)
       it_map_t * itm = IT_DP_MAP (buf->bd_tree, buf->bd_page);
       if (AIO_NATIVE == c_use_aio)
 	{
-	  rc = aio_suspend (&list[inx], 1, NULL);
+	  rc = aio_suspend ((const struct aiocb * const *) &list[inx], 1, NULL);
 	  if (rc) GPF_T1 ("aio_suspend returns error");
 	  rc = aio_return (list[inx]);
 	  if (rc != PAGE_SZ) GPF_T1 ("aio_return error");
