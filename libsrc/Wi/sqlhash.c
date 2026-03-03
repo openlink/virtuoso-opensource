61f18d1f70c77305bd39080055c66d530bf49596
Mitko Iliev <imitko@openlinksw.com>
2026-03-03 14:52:46 +0000

    Fixed if GEO const use ANY type for result

--- libsrc/Wi/sqlhash.c
+++ libsrc/Wi/sqlhash.c
@@ -56,6 +56,11 @@ key_col_from_ssl (dbe_key_t * key, state_slot_t * ssl, int quietcast, int op)
   col->col_sqt.sqt_non_null = 0;
   if (DV_ARRAY_OF_POINTER == col->col_sqt.sqt_dtp)
     col->col_sqt.sqt_dtp = DV_ANY;
+  if (DV_GEO == col->col_sqt.sqt_dtp && SSL_CONSTANT == ssl->ssl_type)
+    {
+      col->col_sqt.sqt_dtp = DV_ANY;
+      col->col_sqt.sqt_col_dtp = DV_ANY;
+    }
   if (DV_LONG_INT == ssl->ssl_dtp /*&& !ssl->ssl_column*/)
     {
       col->col_sqt.sqt_col_dtp = col->col_sqt.sqt_dtp = DV_INT64; /* temp results of int exprs can be wider */
